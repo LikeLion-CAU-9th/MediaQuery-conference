7dbf80fae87ced4e4dc4e13a849a01107f0e389d
youngkwon02 <youngkwon02@naver.com>
2021-09-03 00:59:58 +0900

    feat : basic desktop version

--- responsive-programming/asset/js/main.js
+++ responsive-programming/asset/js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener('scroll', () => {
   let top = document.documentElement.scrollTop;
   if(top > 290) {
-    document.querySelector('header div').style.display = "none";
+    document.querySelector('header div').style.opacity = 0;
+  } else {
+    document.querySelector('header div').style.opacity = 1;
   }
 });
