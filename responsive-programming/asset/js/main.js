603dac7b1f8925787c458d25d06ee7d752b7c0c4
youngkwon02 <youngkwon02@naver.com>
2021-09-05 00:38:14 +0900

    feat : lion appear action

--- responsive-programming/asset/js/main.js
+++ responsive-programming/asset/js/main.js
@@ -2,11 +2,17 @@ document.addEventListener('scroll', () => {
   let top = document.documentElement.scrollTop;
   if(top > 290) {
     document.querySelector('header div').style.opacity = 0;
+    document.querySelector('.lion').style.opacity = 0;
     document.querySelector('nav').style.backgroundColor = 'rgba(0, 0, 0, .7)';
     document.querySelector('nav').style.color = 'white';
   } else {
     document.querySelector('header div').style.opacity = 1;
+    document.querySelector('.lion').style.opacity = 1;
     document.querySelector('nav').style.backgroundColor = 'rgba(255, 255, 255, .4)';
     document.querySelector('nav').style.color = 'black';
   }
 });
+
+window.onload = () => {
+  document.querySelector('.lion').style.left = "60vw";
+}
